5cfbe7d36562d06933f35a400799f46006a73c41
gronbuske <gronbuske@hotmail.com>
2018-01-12 09:56:08 +0100

    Full initial commit

---
+++ TodoApp/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace TodoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 500);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(70, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Check all";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(403, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "All",
+            "Active",
+            "Completed"});
+            this.comboBox1.Location = new System.Drawing.Point(15, 536);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(154, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 520);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoScroll = true;
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.774194F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 93.22581F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 90F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(15, 39);
+            this.tableLayoutPanel1.MaximumSize = new System.Drawing.Size(400, 455);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.83914F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(400, 25);
+            this.tableLayoutPanel1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 578);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Todo App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ TodoApp/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace TodoApp
+{
+    public partial class Form1 : Form
+    {
+        public struct rowControls
+        {
+            public CheckBox cb;
+            public TextBox text;
+            public Button deleteButton;
+        }
+
+        public List<rowControls> rowList;
+
+        public Form1()
+        {
+            rowList = new List<rowControls>();
+
+            InitializeComponent();
+            comboBox1.SelectedItem = comboBox1.Items[0];
+            textBox1.KeyUp += TextBoxKeyUp;
+            label1.Text = "";
+
+            tableLayoutPanel1.RowCount = 0;
+            tableLayoutPanel1.RowStyles.Clear();
+            tableLayoutPanel1.AutoScroll = true;
+        }
+        
+
+        private void UpdateTable()
+        {
+            tableLayoutPanel1.Controls.Clear();
+            tableLayoutPanel1.RowCount = rowList.Count;
+            int nrNotChecked = 0;
+            int index = comboBox1.SelectedIndex;
+            int counter = 0;
+            for (int i = 0; i < rowList.Count; i++)
+            {
+                if (rowList[i].cb.Checked == false)
+                    nrNotChecked++;
+                if (index == 1 && rowList[i].cb.Checked)
+                    continue;
+                else if (index == 2 && !rowList[i].cb.Checked)
+                    continue;
+                tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30));
+                tableLayoutPanel1.Controls.Add(rowList[i].cb, 0, counter);
+                tableLayoutPanel1.Controls.Add(rowList[i].text, 1, counter);
+                tableLayoutPanel1.Controls.Add(rowList[i].deleteButton, 2, counter);
+                counter++;
+            }
+            label1.Text = nrNotChecked.ToString() + " things left to do";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                foreach (var v in rowList)
+                    v.cb.Checked = true;
+            }
+            else
+            {
+                foreach (var v in rowList)
+                    v.cb.Checked = false;
+            }
+            UpdateTable();
+        }
+        
+        private void itemChecked(object sender, EventArgs e)
+        {
+            UpdateTable();
+        }
+
+        private void RemoveButtonPressed(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            for (int i = 0; i < rowList.Count; i++)
+            {
+                if (b == rowList[i].deleteButton)
+                {
+                    rowList.RemoveAt(i);
+                    break;
+                }
+            }
+            UpdateTable();
+        }
+
+        private void itemNameChange(object sender, EventArgs e)
+        {
+            TextBox l = (TextBox)sender;
+            l.ReadOnly = false;
+        }
+
+        private void finishedEditingName(object sender, EventArgs e)
+        {
+            ((TextBox)sender).ReadOnly = true;
+            rowList.Sort((s1, s2) => String.Compare(s1.text.Text, s2.text.Text));
+            UpdateTable();
+        }
+
+        private void checkForEnter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ((TextBox)sender).ReadOnly = true;
+                rowList.Sort((s1, s2) => String.Compare(s1.text.Text, s2.text.Text));
+                UpdateTable();
+            }
+        }
+
+        private void TextBoxKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && !string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                CheckBox newcb = new CheckBox() { Text = "" };
+                newcb.CheckedChanged += itemChecked;
+                Button newB = new Button() { Text = "Remove" };
+                newB.Click += RemoveButtonPressed;
+                TextBox newL = new TextBox() { Text = textBox1.Text };
+                newL.DoubleClick += itemNameChange;
+                newL.ReadOnly = true;
+                newL.LostFocus += finishedEditingName;
+                newL.KeyUp += checkForEnter;
+                rowList.Add(new rowControls()
+                {
+                    cb = newcb,
+                    text = newL,
+                    deleteButton = newB
+                });
+                textBox1.Clear();
+                rowList.Sort((s1, s2) => String.Compare(s1.text.Text, s2.text.Text));
+                UpdateTable();
+                e.Handled = true;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateTable();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e){}
+        private void label1_Click(object sender, EventArgs e){}
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e) { }
+    }
+}
